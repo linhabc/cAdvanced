8d2fc85b714e5eb552bd16b12b5ee465244c46b2
nguyen le ngoc linh <jonathanrocrach2@gmail.com>
2018-10-21 23:12:19 +0700

    add GraphJRB.c

--- week6/graphJRB.c
+++ week6/graphJRB.c
@@ -7,33 +7,56 @@ Graph createGraph(){
 }
 
 void addEdge(Graph graph, int v1, int v2){
-  Graph g1 = jrb_find_int(v1);
-  Graph G2 = jrb_find_int(v2);
-  Graph newGraph = createGraph();
+  JRB tree = make_jrb();
 
-  if(g1==NULL && g2==NULL){
-    jrb_insert_int(g, v1, new_jval_v(newGraph));
-    jrb_insert_int(newGraph, v2, new_jval_i(1));
-  }else if(g1==NULL){
-    
-  }else if(g2==NULL){
-
+  Graph g1 = jrb_find_int(graph, v1);            //find if key v1 exist
+  if(g1==NULL){
+    tree = creatGraph();                         //create new tree
+    jrb_insert_int(graph, v1, new_jval_v(tree)); //insert new tree as value of key v1
   }else{
-   
+    tree = (JRB)jval_v(g1->val);                 //else, assign new tree as v1 edge tree
   }
+  jrb_insert_int(tree, v2, new_jval_i(1));       //insert value for node in newtree
+
+  Graph g2 = jrb_find_int(graph, v2);            //similar as v1
+  if(g2==NULL){
+    tree = creatGraph();
+    jrb_insert_int(graph, v2, new_jval_v(tree));
+  }else{
+   tree = (JRB)jval_v(g2->val);
+  }
+  jrb_insert_int(tree, v1, new_jval_i(1));
 }
 
 int adjacent(Graph graph, int v1, int v2){
   
 }
 
-int getAdjacentVertices (Graph graph, int v, int* output);
-
-void printGraph(Graph graph){
-  JRB tmp;
-  jrb_traverse(tmp, graph){
-    printf("\n(%d %d)\n", jval_i(tmp->key), jval_i(tmp->val));
+int getAdjacentVertices (Graph graph, int v, int* output){
+  JRB node;
+  int i=0;
+  node = jrb_find_int(graph, v);
+  tree = (JRB)jval_v(node->val);
+  total = 0;   
+  jrb_traverse(node, tree){
+    output[i++] = jval_i(node->key);
   }
 }
 
-void dropGraph(Graph graph);
+void printGraph(Graph graph){
+  JRB tmp, nodetmp;
+  jrb_traverse(tmp, graph){
+    printf("%d: ", jval_i(tmp->key));
+    jrb_traverse(nodetmp, tmp->val){
+      printf("%d ", jval_i(nodetmp->key));
+    }
+  }
+}
+
+void dropGraph(Graph graph){
+  JRB tmp;
+  jrb_traverse(tmp, graph){
+    jrb_free_tree(tmp->val);
+  }
+  jrb_free_tree(graph);
+}
