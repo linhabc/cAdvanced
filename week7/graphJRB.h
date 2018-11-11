69adc713a2bb4ad7ebf75215e06b7e5a5b3aae11
nguyen le ngoc linh <jonathanrocrach2@gmail.com>
2018-11-11 11:19:57 +0700

    exercise station.c

--- week7/graphJRB.h
+++ week7/graphJRB.h
@@ -10,7 +10,7 @@ Graph createGraph();
 void addEdge(Graph graph, int v1, int v2);
 int adjacent(Graph graph, int v1, int v2);
 int getAdjacentVertices (Graph graph, int v, int* output);
-void printGraph();
+void printGraph(Graph graph);
 void dropGraph(Graph graph);
 void BFS(Graph graph, int start, int stop, void(*func)(int));
 void DFS(Graph graph, int start, int stop, void(*func)(int));
